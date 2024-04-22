8f0e1e08f53578d2d21e26e9dd2b3048eb620f5b
Katiasun <katjatolok3@gmail.com>
2024-04-22 15:10:22 +0200

    add class container and styles for it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Pricing from "./components/Pricing";
 
 export default function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <div className="container"></div>
+    </>
+  );
 }
